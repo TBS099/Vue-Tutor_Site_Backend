eacd4b5f74b45398eee0d964287c82de3f1c3504
Tanush Bikram Shah <109902776+TBS099@users.noreply.github.com>
2025-11-08 22:59:32 +0000

    Update server.js
    Re-created server.js due to mistake in creation of the file from powershell.
    Created basic get route and database connection to check if server.js and db.js is working

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-ÿþ
+import express from "express";
+import { connectToDatabase, getDB } from "./db.js";
+
+const app = express();
+app.use(express.json());
+
+let db;
+
+// Connect to the the database before starting the server
+connectToDatabase((error) => {
+  if (!error) {
+    db = getDB();
+
+    //ROUTES
+    //GET: all lessons
+    app.get("/lessons", async (req, res) => {
+      try {
+        const lessons = await db.collection("lessons").find().toArray();
+        res.status(200).json(lessons);
+      } catch (error) {
+        console.error("Error fetching lessons:", error);
+        res.status(500).json({ error: "Failed to fetch lessons" });
+      }
+    });
+
+    // Start the server
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  } else {
+    console.error("Failed to start server due to database connection error");
+  }
+});
