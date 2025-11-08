4ea0b2071080bf7524f4c7af2973b29f039dd5f8
Tanush Bikram Shah <109902776+TBS099@users.noreply.github.com>
2025-11-08 22:41:02 +0000

    Created lesson class

---
+++ Models/Lesson.js
@@ -0,0 +1,12 @@
+export default class Lesson {
+  constructor(id, subject, location, price, spaces, image, description, rating) {
+    this.id = id;
+    this.subject = subject;
+    this.location = location;
+    this.price = price;
+    this.spaces = spaces;
+    this.image = image;
+    this.description = description;
+    this.rating = rating;
+  }
+}
